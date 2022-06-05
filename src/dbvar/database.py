4d9bfadbf1ae606dd43bc6b26fa9601a0b3e4f6e
Jean-Baptiste Lamouche <jbaptiste.lamouche@gmail.com>
2022-06-05 19:12:05 +0200

    check if CHROM column is valid correct otherwise and update in sqlitedb

--- src/dbvar/database.py
+++ src/dbvar/database.py
@@ -3,16 +3,81 @@ import sqlite3
 
 
 class Databasevar:
-    def __init__(self, df_variants, db, vcfname):
+    def __init__(self, df_variants, db, vcfname, tablename, config):
         self.conn = sqlite3.connect(db)
         self.c = self.conn.cursor()
-        # self.db = df_variants.to_sql(
-        #    "VCF_variants", self.conn, if_exists="replace", index=False
-        # )
         self.vcfname = vcfname
-        pass
+        self.db = db
+        self.df_variants = df_variants
+        self.tablename = tablename
+        self.config = config
+        print("#[INFO] SQLite " + db + " connected ")
 
     def request(self, request):
-        print("#[INFO] SQL REQUEST " + request)
-        self.c.execute(request)
+        try:
+            print("#[INFO] SQL REQUEST " + request)
+            self.c.execute(request)
+            self.conn.commit()
+        except sqlite3.Error as error:
+            print("Failed request " + request, error)
+        finally:
+            if self.conn:
+                self.conn.close()
+                print("#[INFO] The SQLite connection is closed")
+
+    def update(self):
+        pass
+
+    def create_table(self):
+        self.df_variants.to_sql(
+            self.tablename, self.conn, if_exists="replace", index=False
+        )
+
+    def chromosome_check(self):
+        # request = "SELECT * FROM " + self.tablename + " WHERE #CHROM LIKE ", (chr,)
+        # self.request(request)
+        self.c.execute(
+            "SELECT * from " + self.tablename + " WHERE [#CHROM] Like ?",
+            ("chr%",),
+        )
+        res = self.c.fetchall()
+        print(len(res))
+        print(type(self.c.fetchall()))
+        print(len(self.df_variants.index))
+        if len(res) != len(self.df_variants.index):
+            self.c.execute(
+                "SELECT [#CHROM], POS from "
+                + self.tablename
+                + " WHERE [#CHROM] NOT IN ({seq})".format(
+                    seq=",".join(["?"] * len(self.config["variants"]["chr"]))
+                ),
+                self.config["variants"]["chr"],
+            )
+            res_chr = self.c.fetchall()
+            if not res_chr:
+                # print(res_chr)
+                print("#[INFO] Chromosomes check OK")
+            else:
+                print("WARNING some variants are not conform ", res_chr)
+                print("#[INFO] correct CHR field")
+                for values in res_chr:
+                    self.update_value(values)
+            #    for
+
+    def update_value(self, old):
+        self.c.execute(
+            "UPDATE "
+            + self.tablename
+            + " SET [#CHROM] = 'chr"
+            + str(old[0])
+            + "' WHERE [#CHROM] = '"
+            + str(old[0])
+            + "' AND POS = '"
+            + str(old[1])
+            + "'"
+        )
         self.conn.commit()
+        self.c.execute("SELECT * FROM " + self.tablename)
+
+    def db_to_dataframe(self):
+        return self.c.fetchall()
